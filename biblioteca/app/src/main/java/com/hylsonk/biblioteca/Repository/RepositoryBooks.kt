66ee0e1dd34d6b85e4c28288f7956fa442a611a5
Hylson Kobayashi <hylsonk@gmail.com>
2019-10-23 11:59:27 -0300

    removendo duplicatas

--- biblioteca/app/src/main/java/com/hylsonk/biblioteca/Repository/RepositoryBooks.kt
+++ biblioteca/app/src/main/java/com/hylsonk/biblioteca/Repository/RepositoryBooks.kt
@@ -3,7 +3,12 @@ package com.hylsonk.biblioteca.Repository
 import com.hylsonk.biblioteca.Model.Book
 
 class RepositoryBooks {
-    private val books: List<Book>
+    private val books = arrayOf(
+        "Prisioneiros da Mente",
+        "O Poder do Hábito",
+        "Harry Potter e as Relíquias da Morte",
+        "A Guerra dos Tronos"
+    )
 
     fun getBooks(): Array<String>{
         return books
